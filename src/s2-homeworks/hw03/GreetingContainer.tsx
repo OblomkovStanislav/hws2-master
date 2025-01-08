a36e1159fbc8de90b56486e3e873d3cad1277ec9
OblomkovStanislav <oblomkovstanislav@gmail.com>
2025-01-08 16:26:49 +0300

    Initial commit

---
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import Greeting from './Greeting'
+import { UserType } from './HW3'
+
+type GreetingContainerPropsType = {
+    users: UserType[] // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
+}
+
+export const pureAddUser = (name: string, setError: (error: string) => void, setName: (name: string) => void, addUserCallback: (name: string) => void) => {
+    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+    if (name === "") setError("Error");
+    else {
+        addUserCallback(name);
+        setName("");
+    }
+}
+
+export const pureOnBlur = (name: string, setError: (error: string) => void) => { // если имя пустое - показать ошибку
+    if (name === "") setError("Error");
+}
+
+export const pureOnEnter = (e: React.KeyboardEvent<HTMLElement>, addUser: () => void) => { // если нажата кнопка Enter - добавить
+    if (e.key === "Enter") addUser();
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
+    users,
+    addUserCallback,
+}) => {
+    // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+
+    const setNameCallback = (e: React.ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
+        error && setError('')
+    }
+    const addUser = () => {
+        pureAddUser(name, setError, setName, addUserCallback)
+    }
+
+    const onBlur = () => {
+        pureOnBlur(name, setError)
+    }
+
+    const onEnter = (e: React.KeyboardEvent<HTMLElement>) => {
+        pureOnEnter(e, addUser)
+    }
+
+    const totalUsers = users.length > 0 ? users.length : 0; // need to fix
+    const lastUserName = users.length > 0 ? users[users.length-1].name : ""; // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            onBlur={onBlur}
+            onEnter={onEnter}
+            error={error}
+            totalUsers={totalUsers}
+            lastUserName={lastUserName}
+        />
+    )
+}
+
+export default GreetingContainer
